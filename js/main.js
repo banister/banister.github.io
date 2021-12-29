6d9042b75c3548c255f008ac2338fc15f48d165a
John Mair <jrmair@gmail.com>
2021-12-29 08:32:33 +0100

    Fix typo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-// Triggered with: curl -X GET https://en5p4qr3jfyinq1.m.pipedream.net/compile_complete\?name\=rumi \
-// -H 'Authorization: Bearer jasdf3jJuisdfaL7safnsfgmiU7124Hn'
-document.getElementById("MyButton").onclick = function() {
+document.getElementById("MyButton").onclick = => {
     fetch("https://en5p4qr3jfyinq1.m.pipedream.net/compile_complete", {
-        method: "GET",
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Authorization": "Bearer jasdf3jJuisdfaL7safnsfgmiU7124Hn"
