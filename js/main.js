02271f0b941d5d80162cf14d30ef512c9293f703
John Mair <jrmair@gmail.com>
2021-12-29 07:51:59 +0100

    Hit the pipedream endpoint

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// Triggered with: curl -X GET https://en5p4qr3jfyinq1.m.pipedream.net/compile_complete\?name\=rumi \
+// -H 'Authorization: Bearer jasdf3jJuisdfaL7safnsfgmiU7124Hn'
 document.getElementById("MyButton").onclick = function() {
-    alert("hello world!");
+    fetch("https://en5p4qr3jfyinq1.m.pipedream.net/compile_complete", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer jasdf3jJuisdfaL7safnsfgmiU7124Hn"
+        }
+      })
 };
