e11e66ff84b51d6704a43ece1469bceba148bc22
John Mair <jrmair@gmail.com>
2021-12-30 04:18:45 +0100

    Update to newest endpoint url

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.getElementById("MyButton").onclick = function() {
-    fetch("https://en5p4qr3jfyinq1.m.pipedream.net/compile_complete", {
+    fetch("https://en6y8zik2gncezv.m.pipedream.net/send_email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
